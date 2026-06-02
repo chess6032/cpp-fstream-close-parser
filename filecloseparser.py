e99e90254b399d1f8e170a245a0ecd98adbd6c0a
Caleb Hessing <cckkhh7@gmail.com>
2026-06-02 14:05:55 -0600

    stubbing out some stuff

--- filecloseparser.py
+++ filecloseparser.py
@@ -1,6 +1,39 @@
 import sys
 from io import TextIOWrapper
+import re
+from typing import Callable
 
+class FileCloseParser:
+    def __init__(self, file: TextIOWrapper):
+        self.file = file
+        self.fstreams = []
+
+    class Fstream:
+        def __init__(self, name:str):
+            self.name = name
+
+    # static methods that return a function that does some regex shih
+
+    @staticmethod
+    def match_with_declaration(line: str) -> Callable:
+        return lambda: None
+
+    @staticmethod
+    def match_with_open(line: str) -> Callable:
+        return lambda: None
+
+    @staticmethod
+    def match_with_close(line: str) -> Callable:
+        return lambda: None
+
+    # the actual parsing & checking stuff for fstream closing
+
+    def walk_through_file(self) -> None:
+        for line in self.file:
+            self.process_line(line.rstrip())
+
+    def process_line(self, line:str) -> None:
+        pass
 
 
 if __name__ == "__main__":
@@ -10,7 +43,7 @@ if __name__ == "__main__":
 
     result = None
     with open(sys.argv[1]) as f:
-        pass # TODO
+        parser = FileCloseParser(f)
 
     if result:
         sys.exit(0)
